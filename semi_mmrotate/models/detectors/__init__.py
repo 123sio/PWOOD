db240d2c0830c12aa106e8b57d83bccbe11a1a3f
123sio <1803897165@qq.com>
2025-05-23 11:30:03 +0800

    add

---
+++ semi_mmrotate/models/detectors/__init__.py
@@ -0,0 +1,4 @@
+from .semi_rotated_fcos import SemiRotatedFCOS
+from .semi_rotated_fasterrcnn import SemiRotatedFasterRCNN
+from .semi_rotated_retinanet import SemiRotatedRetinaNet
+from .semi_point2rboxv2 import SemiPoint2RBoxV2
